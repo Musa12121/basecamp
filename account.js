3daa4da2cfa15bfdac9fa5402b80d6a65d011144
abdullayeva821 <abdullayevaaysu821@gmail.com>
2026-06-19 21:24:13 +0400

    added js to account page and trying to write the default page after logging in

---
+++ account.js
@@ -0,0 +1,6 @@
+let button=document.querySelector('.newprojectbtn')
+let newprojectpage=document.querySelector('.newprojectpage')
+
+button.addEventListener('click',()=>{
+    newprojectpage.classList.toggle("disappear");    
+})
