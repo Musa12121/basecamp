a558b6547000db0d7738a9cfb87e51b3e8cc13fa
abdullayeva821 <abdullayevaaysu821@gmail.com>
2026-06-22 21:28:41 +0400

    save

---
+++ account.js
@@ -0,0 +1,9 @@
+let button=document.querySelector('.newprojectbtn')
+let newprojectpage=document.querySelector('.newprojectpage')
+let defaultpage=document.querySelector('.default')
+
+button.addEventListener('click',()=>{
+    newprojectpage.classList.toggle("disappear");   
+    defaultpage.classList.toggle("disappear");
+
+})
