0b7c5734d65cfd7fad02c43e2b5635caa9ff8d13
abdullayeva821 <abdullayevaaysu821@gmail.com>
2026-06-22 21:19:29 +0400

    trying to do responsive

--- account.js
+++ account.js
@@ -1,6 +1,8 @@
 let button=document.querySelector('.newprojectbtn')
 let newprojectpage=document.querySelector('.newprojectpage')
+let defaultpage=document.querySelector('.default')
 
 button.addEventListener('click',()=>{
-    newprojectpage.classList.toggle("disappear");    
+    newprojectpage.classList.toggle("disappear");   
+    defaultpage.classList.toggle("disappear");
 })
