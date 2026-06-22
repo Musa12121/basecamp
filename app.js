6986d622bb259b18b16860f91527190415a02d9e
Amin-Asadullayev <aminvit693@gmail.com>
2026-06-22 21:59:58 +0400

    /new endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 process.loadEnvFile()
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
 
 app.listen(process.env.PORT, () => {
     console.log("App launched")
