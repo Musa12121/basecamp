7ee67091afd4663fabdc2ea565e442084c681af2
user <usernameqwert3@gmail.com>
2026-06-23 17:11:02 +0400

    started to write js for adding new projects

--- public/account.js
+++ public/account.js
@@ -2,7 +2,16 @@ let button=document.querySelector('.newprojectbtn')
 let newprojectpage=document.querySelector('.newprojectpage')
 let defaultpage=document.querySelector('.default')
 
+let addProjectBtn=document.getElementById('newProjectButton').value
+
 button.addEventListener('click',()=>{
     newprojectpage.classList.toggle("disappear");   
     defaultpage.classList.toggle("disappear");
 })
+
+addProjectBtn.addEventListener('click',()=>{
+let projectName=document.getElementById('newProjectName').value
+let projectInfo=document.getElementById('newProjectDescription').value
+
+})
+
