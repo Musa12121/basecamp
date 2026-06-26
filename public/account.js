6b008eaf5f04fc1d1656cd2e45520154475af986
user <usernameqwert3@gmail.com>
2026-06-27 01:51:02 +0400

    birinci struktur

--- public/account.js
+++ public/account.js
@@ -1,25 +1,25 @@
-let button = document.querySelector('.newprojectbtn')
-let newprojectpage = document.querySelector('.newprojectpage')
-let defaultpage = document.querySelector('.default')
-let projectList = document.querySelector('.projectList')
-let addProjectBtn = document.getElementById('newProjectButton')
-let settingsPage=document.querySelector('.settingsPage')
+let button = document.querySelector(".newprojectbtn");
+let newprojectpage = document.querySelector(".newprojectpage");
+let defaultpage = document.querySelector(".default");
+let projectList = document.querySelector(".projectList");
+let addProjectBtn = document.getElementById("newProjectButton");
+let settingsPage = document.querySelector(".settingsPage");
 
-button.addEventListener('click', () => {
-    let projectName = document.getElementById('newProjectName')
-    let projectInfo = document.getElementById('newProjectdDescription')
-    newprojectpage.classList.toggle("disappear");
-    defaultpage.classList.toggle("disappear");
-    projectName.value = '';
-    projectInfo.value = '';
-})
+button.addEventListener("click", () => {
+  let projectName = document.getElementById("newProjectName");
+  let projectInfo = document.getElementById("newProjectdDescription");
+  newprojectpage.classList.toggle("disappear");
+  defaultpage.classList.toggle("disappear");
+  projectName.value = "";
+  projectInfo.value = "";
+});
 
-addProjectBtn.addEventListener('click', () => {
-    let projectName = document.getElementById('newProjectName')
-    let projectInfo = document.getElementById('newProjectdDescription')
-    let card = document.createElement("li")
-    card.classList.add("project")
-    card.innerHTML = `<div class="newProjectPart1">
+addProjectBtn.addEventListener("click", () => {
+  let projectName = document.getElementById("newProjectName");
+  let projectInfo = document.getElementById("newProjectdDescription");
+  let card = document.createElement("li");
+  card.classList.add("project");
+  card.innerHTML = `<div class="newProjectPart1">
                         <div>
                             <h3 id="title">${projectName.value}</h3>
                             <div>
@@ -36,18 +36,23 @@ addProjectBtn.addEventListener('click', () => {
                         <div><i class="fa-solid fa-user-group"></i><span class="userCount">1</span>
                         <i class="fa-solid fa-comments"></i><span class="commentCount">0</span></div>
                         <i class="fa-regular fa-trash-can delete" style="color: rgb(255, 255, 255);"></i></div>`;
-    projectList.appendChild(card);
-    newprojectpage.classList.toggle("disappear");
-    defaultpage.classList.toggle("disappear");
-    projectName.value = '';
-    projectInfo.value = '';
-})
+  projectList.appendChild(card);
+  newprojectpage.classList.toggle("disappear");
+  defaultpage.classList.toggle("disappear");
+  projectName.value = "";
+  projectInfo.value = "";
+});
 
-projectList.addEventListener('click',e=>{
-    if(e.target.classList.contains("delete")){
-        e.target.parentElement.parentElement.remove();
+projectList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.parentElement.remove();
+  } else if (e.target.classList.contains("settings")) {
+    settingsPage.classList.remove("disappear");
+    if (!newprojectpage.classList.contains("disappear")) {
+      newprojectpage.classList.add("disappear");
     }
-    else if(e.target.classList.contains("settings")){
-        settingsPage.style.display='flex';
+    if (!defaultpage.classList.contains("disappear")) {
+      defaultpage.classList.add("disappear");
     }
-})
+  }
+});
